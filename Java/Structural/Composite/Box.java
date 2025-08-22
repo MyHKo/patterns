5af246c728e6fc6102b1b05d27b0017a966beb61
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-08-22 22:20:19 +0200

    Add box class and add codestyle fixes

---
+++ Java/Structural/Composite/Box.java
@@ -0,0 +1,17 @@
+class Box extends Item {
+    ArrayList<Item> productList;
+
+    public Box() {}
+
+    public void addItem(Item product) {
+        this.productList.add(product)
+    }
+
+    @Override
+    public int getPrice() {
+        int price = 0;
+        for(int i = 0; i < productList.length(); i++){
+            price += productList.get(i).getPrice();
+        }
+    }
+}
