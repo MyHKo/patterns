6df271c40f39ebdee5fbe31150416ac94ab0a424
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-09-24 10:59:28 +0200

    Add Testing to the Adapter pattern

--- Java/Structural/Adapter/AdapterTest.java
+++ Java/Structural/Adapter/AdapterTest.java
@@ -1,4 +1,4 @@
-public class AdapterTest{
+public class AdapterTest {
     public static void main(String[] args) {
         MusicPlayerAdapter player = new MusicPlayerAdapter();
         player.play("Music.mp3");
